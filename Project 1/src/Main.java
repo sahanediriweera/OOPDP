2d439329e164e0b44b529c79dc7985cd669412a4
SCARRAIDER <rsahanediriweera@gmail.com>
2023-01-17 00:44:49 +0530

    completed the first project of basic java

--- Project 1/src/Main.java
+++ Project 1/src/Main.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //read all the args
+        //select operator and operands
+        //covert operands to double
+        //if min calculate min
+        //if max calculate max
+        //print result
+
+        String operator;
+        List<Double> operands = new ArrayList<>();
+
+        operator = args[0];
+
+        for(int i = 1;i<args.length;i++){
+            operands.add(Double.parseDouble(args[i]));
+        }
+
+        double result = operands.get(0);
+        if(operator.equals("min")){
+            for(int i = 1;i<operands.size();i++){
+                result  = Math.min(result,operands.get(i));
+            }
+        } else if (operator.equals("max")){
+            for(int i = 1;i<operands.size();i++){
+                result = Math.max(result,operands.get(i));
+            }
+        } else if (operator.equals("sum")) {
+            for (int i = 1; i<operands.size();i++){
+                result+= operands.get(i);
+            }
+        }
+
+        System.out.println(result);
+
     }
 }
