a856a7b17aaeaf09c0308c4433c674e93b066f7a
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-06 14:22:40 +0530

    partial completion of jButtonDemo

---
+++ SwingPart 6/src/JButtonDemo.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class JButtonDemo implements ActionListener {
+
+    JLabel jLabel;
+
+    public JButtonDemo(){
+
+       JFrame jFrame = new JFrame("JButtonDemo");
+       jFrame.setLayout(new FlowLayout());
+       jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       jFrame.setSize(500,400);
+
+       ImageIcon hourGlass = new ImageIcon("hourglass.png");
+       JButtonDemo jButtonDemo
+
+
+    }
+}
