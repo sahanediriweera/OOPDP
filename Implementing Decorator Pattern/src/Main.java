dc4913f177ab9651ee183454f7ef9cd28ce2314c
SCARRAIDER <rsahanediriweera@gmail.com>
2023-05-21 16:34:43 +0530

    completed implementing decorator pattern

--- Implementing Decorator Pattern/src/Main.java
+++ Implementing Decorator Pattern/src/Main.java
@@ -1,5 +1,19 @@
+import Decorators.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String salaryRecords = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000";
+        DataSourceDecorator encoded = new CompressionDecorator(
+                new EncryptionDecorator(
+                        new FileDataSource("out/OutputDemo.txt")));
+        encoded.writeData(salaryRecords);
+        DataSource plain = new FileDataSource("out/OutputDemo.txt");
+
+        System.out.println("- Input ----------------");
+        System.out.println(salaryRecords);
+        System.out.println("- Encoded --------------");
+        System.out.println(plain.readData());
+        System.out.println("- Decoded --------------");
+        System.out.println(encoded.readData());
     }
 }
