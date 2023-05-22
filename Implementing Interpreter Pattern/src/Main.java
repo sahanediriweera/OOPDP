5d2828b4c6286a190e8181932f9362786937a281
SCARRAIDER <rsahanediriweera@gmail.com>
2023-05-22 10:16:44 +0530

    completed interpreter pattern implementatoin

--- Implementing Interpreter Pattern/src/Main.java
+++ Implementing Interpreter Pattern/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+    public static Expression getMaleExpression(){
+        Expression robert = new TerminalExpression("Robert");
+        Expression john = new TerminalExpression("John");
+        return new OrExpression(robert,john);
+    }
+
+    public static Expression getMarriedWomenExpression(){
+        Expression julie = new TerminalExpression("Julie");
+        Expression married = new TerminalExpression("Married");
+
+        return new AndExpression(julie,married);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWomen = getMarriedWomenExpression();
+
+        System.out.println("John is male? " + isMale.interpret("John"));
+        System.out.println("Julie is a married women? " + isMarriedWomen.interpret("Married Julie"));
     }
 }
